e3ba92815fad2344c8c7b2d31dad13026c0fa885
rafapaezbas <rafaelpaezbastida@gmail.com>
2025-08-01 19:36:32 +0200

    adapted test for Windows

--- test/index.test.js
+++ test/index.test.js
@@ -2,9 +2,14 @@ const test = require('brittle')
 const tmp = require('test-tmp')
 const createIPC = require('..')
 const path = require('path')
+const { isWindows } = require('which-runtime')
 
 test('create ipc client', async (t) => {
   const dir = await tmp()
   const ipc = createIPC(dir)
-  t.is(ipc._socketPath, path.join(dir, 'pear.sock'))
+  if (isWindows) {
+    t.ok(ipc._socketPath.includes('.pipe'))
+  } else {
+    t.is(ipc._socketPath, path.join(dir, 'pear.sock'))
+  }
 })
